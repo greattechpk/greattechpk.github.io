c8aa09fd90e595272ab1ed0a30c83144b4c620c5
greattechpk <great.techpk@gmail.com>
2020-06-22 19:02:03 -0400

    added fullscreen/compress button

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,11 +1,16 @@
 let isFS = false
 var goFS = document.getElementById("goFS");
+let iconFS = document.getElementById('iconFS')
 
 goFS.addEventListener("click", function () {
     if (!isFS) {
         document.documentElement.requestFullscreen();
+        iconFS.classList.remove("fa-expand-alt")
+        iconFS.classList.add("fa-compress-alt")
     } else {
-          document.exitFullscreen();
-        }
+        document.exitFullscreen();
+        iconFS.classList.remove("fa-compress-alt")
+        iconFS.classList.add("fa-expand-alt")
+    }
     isFS = !isFS
 }, false);
